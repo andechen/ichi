0259bb10d715c14e66b1c66f15e35ce4e43ba4dd
Ande Chen <andechen2000@gmail.com>
2023-03-16 03:41:20 -0400

    socket programming

--- client/bt_client.py
+++ client/bt_client.py
@@ -6,27 +6,7 @@ from multiprocessing import Process
 
 #! python3 -m pip install -r requirements.txt
 
-# HOST DEVICE INFORMATION
-host_addr = "7C:50:79:3E:8F:2C"     # Host PC's MAC address
-port = 4                            # Connect to COM4
-
-# SET UP CONNECTION
-s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.connect((host_addr, port))
-print("CONNECTION ESTABLISHED WITH " + host_addr + " PORT " + str(port) + "...")
-
-# SETUP GPIO PINS
-pin_MB_L = digitalio.DigitalInOut(board.D22)
-pin_MB_L.direction = digitalio.Direction.INPUT
-pin_MB_L.pull = digitalio.Pull.UP
-mb_l = Debouncer(pin_MB_L)
-
-pin_MB_R = digitalio.DigitalInOut(board.D4)
-pin_MB_R.direction = digitalio.Direction.INPUT
-pin_MB_R.pull = digitalio.Pull.UP
-mb_r = Debouncer(pin_MB_R)
-
-# PROCESS TASKS
+#################### PROCESS TASKS ####################
 def button_listener(button):
     button_stream = ""
 
@@ -44,21 +24,49 @@ def button_listener(button):
             s.send(button_stream.encode())
             button_stream = ""
 
-# SETUP MULTI-PROCESSING
-# processlist = []
-# processlist.append(Process(target=button_listener(mb_l)))
-# processlist.append(Process(target=button_listener(mb_r)))
+#################### HOST DEVICE INFORMATION ####################
+host_addr = "7C:50:79:3E:8F:2C"     # Host PC's MAC address
+port = 4                            # Connect to COM4
 
-# for p in processlist:
-#     p.start()
+# SET UP CONNECTION
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.connect((host_addr, port))
+print("CONNECTION ESTABLISHED WITH " + host_addr + " PORT " + str(port))
 
-# for p in processlist:
-#     p.join()
+#################### SETUP GPIO PINS ####################
+# LEFT CLICK BUTTON
+pin_MB_L = digitalio.DigitalInOut(board.D22)
+pin_MB_L.direction = digitalio.Direction.INPUT
+pin_MB_L.pull = digitalio.Pull.UP
+mb_l = Debouncer(pin_MB_L)
+
+# RIGHT CLICK BUTTON
+pin_MB_R = digitalio.DigitalInOut(board.D4)
+pin_MB_R.direction = digitalio.Direction.INPUT
+pin_MB_R.pull = digitalio.Pull.UP
+mb_r = Debouncer(pin_MB_R)
+
+# MIDDLE CLICK BUTTON
+pin_MB_M = digitalio.DigitalInOut(board.D4)     #TODO: Update joystick select pin number
+pin_MB_M.direction = digitalio.Direction.INPUT
+pin_MB_M.pull = digitalio.Pull.UP
+mb_m = Debouncer(pin_MB_M)
+
+#################### SETUP MULTI-PROCESSING ####################
+processlist = []
+processlist.append(Process(target=button_listener(mb_l)))
+processlist.append(Process(target=button_listener(mb_r)))
+
+for p in processlist:
+    p.start()
+
+for p in processlist:
+    p.join()
 
 # Send data
 try:
     while True:
-        button_listener(mb_l)
+        # button_listener(mb_l)
 
 except KeyboardInterrupt:
     s.close()
