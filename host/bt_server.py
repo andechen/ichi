6465d7a78a4dae8f10c6338a9402e9b3c7f998fa
Ande Chen <andechen2000@gmail.com>
2023-03-16 19:26:20 -0400

    socket programming

--- host/bt_server.py
+++ host/bt_server.py
@@ -5,28 +5,30 @@ import pyautogui
 s = serial.Serial('COM4')
 
 def parse_packet(pkt_array):
+    print(pkt_array)
+
     match pkt_array[0]:
         case "MBL":
-            if pkt_array[1] == 1:
+            if pkt_array[1] == "1":
                 pyautogui.mouseDown(button='left')
-            elif pkt_array[1] == 0:
+            elif pkt_array[1] == "0":
                  pyautogui.mouseUp(button='left')
         case "MBR":
-            if pkt_array[1] == 1:
+            if pkt_array[1] == "1":
                 pyautogui.mouseDown(button='right')
-            elif pkt_array[1] == 0:
+            elif pkt_array[1] == "0":
                  pyautogui.mouseUp(button='right')
         case "MBM":
-            if pkt_array[1] == 1:
+            if pkt_array[1] == "1":
                 pyautogui.mouseDown(button='middle')
-            elif pkt_array[1] == 0:
+            elif pkt_array[1] == "0":
                  pyautogui.mouseUp(button='middle')
         case "PTT":
             print("PTT")
 
 while True:
-    recvd_packet = s.readline()
-    print(recvd_packet)
+    rec = s.readline()
+    recvd_packet = rec.decode()
 
     split_pkt = recvd_packet.split("$")
     parse_packet(split_pkt)
